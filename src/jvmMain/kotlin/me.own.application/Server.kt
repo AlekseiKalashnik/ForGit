fa62e4624f8b396212e1877a07f1651767011caf
AlekseiKalashnik <id50538996@gmail.com>
2022-10-30 09:55:30 +0300

    "Add boys in test add through cherry-pick"

--- src/jvmMain/kotlin/me.own.application/Server.kt
+++ src/jvmMain/kotlin/me.own.application/Server.kt
@@ -15,7 +15,7 @@ fun HTML.index() {
     }
     body {
         div {
-            +"Hello from Ktor girls for logs"
+            +"Hello from Ktor bi girls and boys"
         }
         div {
             id = "root"
